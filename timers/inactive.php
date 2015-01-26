f7a31761d5f7bbf8cf619e4e1a6415edc0c60b16
Cees-Jan Kiewiet <ceesjank@gmail.com>
2015-01-26 20:37:08 +0100

    Added inactive example

---
+++ timers/inactive.php
@@ -0,0 +1,20 @@
+<?php
+
+require '../vendor/autoload.php';
+
+$loop = React\EventLoop\Factory::create();
+
+$i = 0;
+$timer = $loop->addTimer(1, function() {
+    echo 'Timer done', PHP_EOL;
+});
+
+$loop->addTimer(3, function () use ($timer, $loop) {
+    if ($loop->isTimerActive($timer)) {
+        echo 'Timer active', PHP_EOL;
+    } else {
+        echo 'Timer inactive', PHP_EOL;
+    }
+});
+
+$loop->run();
