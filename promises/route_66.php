01ff3dd19956913274f28d7f547d53b0d58ed1b6
Cees-Jan Kiewiet <ceesjank@gmail.com>
2015-02-02 10:57:48 +0100

    Timer example

---
+++ promises/route_66.php
@@ -0,0 +1,23 @@
+<?php
+
+require '../vendor/autoload.php';
+
+$i = 0;
+$loop = \React\EventLoop\Factory::create();
+$deferred = new \React\Promise\Deferred();
+
+$timer = $loop->addPeriodicTimer(0.01, function(\React\EventLoop\Timer\Timer $timer) use (&$i, $deferred) {
+	$deferred->notify($i++);
+	if ($i >= 66) {
+		$timer->cancel();
+		$deferred->resolve();
+	}
+});
+
+$deferred->promise()->then(function($i) {
+	echo 'Done!', PHP_EOL;
+}, null, function($i) {
+	echo $i, PHP_EOL;
+});
+
+$loop->run();
